5a7502f7007790cf7ab7acb526de976df648297e
shahensha12 <skshahensha12@gmail.com>
2020-04-16 17:31:10 +0530

    actuator project

---
+++ testdemoactuator/src/main/java/com/shahensha/testdemoactuator/TestdemoactuatorApplication.java
@@ -0,0 +1,13 @@
+package com.shahensha.testdemoactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdemoactuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdemoactuatorApplication.class, args);
+	}
+
+}
---
+++ testdemoactuator/src/test/java/com/shahensha/testdemoactuator/TestdemoactuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahensha.testdemoactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdemoactuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
